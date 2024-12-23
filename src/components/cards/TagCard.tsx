7498e8224a4fd12355cd4857d1cc9ea8d3bf654c
zakaria <zakariasalaheddine777@gmail.com>
2024-12-23 10:46:07 +0100

    Implement Tag Input

--- src/components/cards/TagCard.tsx
+++ src/components/cards/TagCard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import ROUTES from "@/constants/routes";
 import { Badge } from "@/components/ui/badge";
 import { getDeviconsClassName } from "@/src/lib/utils";
+import Image from "next/image";
 
 interface Props {
   _id: string;
@@ -10,24 +11,67 @@ interface Props {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+const TagCard = ({
+  _id,
+  name,
+  questions,
+  showCount,
+  compact,
+  remove,
+  isButton,
+  handleRemove,
+}: Props) => {
+  const iconClass = getDeviconsClassName(name);
 
-    const iconClass = getDeviconsClassName(name);
-  return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2 ">
-      <Badge className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase ">
+  const handleClick = (e:React.MouseEvent) => {
+    e.preventDefault();
+  }
+
+  const content = (
+    <>
+      <Badge className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase flex flex-row gap-2">
         <div className="flex-center space-x-2">
-          <i className={`${iconClass} text-sm`}>
-            
-          </i>
-          <span> {name}</span>
+          <i className={`${iconClass} text-sm`}></i>
+          <span>{name}</span>
         </div>
+        {remove && (
+          <Image
+            src="/icons/close.svg"
+            alt="close"
+            width={12}
+            height={12}
+            className="cursor-pointer invert-0 dark:invert"
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{questions}</p>
+      )}
+    </>
+  );
 
-      {showCount && <p className="small-medium text-dark500_light700">{questions}</p> }
-    </Link>
+  if (compact) {
+    return isButton ? (
+      <button onClick={handleClick} className="flex justify-between gap-2">
+        {content}
+      </button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+        {content}
+      </Link>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      {content}
+    </div>
   );
 };
 
--- src/components/forms/QuestionForm.tsx
+++ src/components/forms/QuestionForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Path, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { AskQuestionSchema } from "@/src/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRef } from "react";
@@ -17,16 +17,16 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import dynamic from "next/dynamic";
 import { MDXEditorMethods } from "@mdxeditor/editor";
+import { z } from "zod";
+import TagCard from "../cards/TagCard";
+
+const Editor = dynamic(() => import("@/src/components/editor/index"), {
+  ssr: false,
+});
+
 const QuestionForm = () => {
-
-  // This is the only place InitializedMDXEditor is imported directly.
-const Editor = dynamic(() => import('@/src/components/editor/index'), {
-  // Make sure we turn SSR off
-  ssr: false
-})
-
   const editorRef = useRef<MDXEditorMethods>(null);
-  const form = useForm({
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
       title: "",
@@ -35,9 +35,48 @@ const Editor = dynamic(() => import('@/src/components/editor/index'), {
     },
   });
 
-  const handleCreateQuestion = (data) => {
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: { value: string[] }
+  ) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const tagInput = e.currentTarget.value.trim();
+
+      if (tagInput && tagInput.length < 15 && !field.value.includes(tagInput)) {
+        form.setValue("tags", [...field.value, tagInput]);
+        e.currentTarget.value = "";
+        form.clearErrors("tags");
+      } else if (tagInput.length > 15) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag length should be less than 15 characters",
+        });
+      } else if (field.value.includes(tagInput)) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag already exists",
+        });
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: { value: string[] }) => {
+    const newTags = field.value.filter((t) => tag !== t);
+    form.setValue("tags", newTags);
+
+    if (newTags.length === 0) {
+      form.setError("tags", {
+        type: "manual",
+        message: "Tags are required",
+      });
+    }
+  };
+
+  const handleCreateQuestion = (data: any) => {
     console.log(data);
   };
+
   return (
     <Form {...form}>
       <form
@@ -54,13 +93,13 @@ const Editor = dynamic(() => import('@/src/components/editor/index'), {
               </FormLabel>
               <FormControl>
                 <Input
-                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px]  border"
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
                   {...field}
                 />
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2.5">
                 Be specific and imagine you’re asking a question to another
-                person
+                person.
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -72,58 +111,69 @@ const Editor = dynamic(() => import('@/src/components/editor/index'), {
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
               <FormLabel className="paragraph-semibold text-dark400_light800">
-                Detailed explanation of your problem <span className="text-primary-500">*</span>
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl>
-                <Editor value={field.value} editorRef={editorRef} fieldChange={field.onchange} />
-                 
+                <Editor
+                  value={field.value}
+                  editorRef={editorRef}
+                  fieldChange={field.onChange}
+                />
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2.5">
-                Introduce the problem and expand on what you’ve put in the title.
-
+                Introduce the problem and expand on what you’ve put in the
+                title.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
-        />{" "}
+        />
         <FormField
           control={form.control}
           name="tags"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col gap-3">
               <FormLabel className="paragraph-semibold text-dark400_light800">
-               Tags <span className="text-primary-500">*</span>
+                Tags <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl>
-
                 <div>
-                <Input
-                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px]  border"
-                  {...field}
-                  placeholder="Add tags..."
-                />
-
-                Tags
-
+                  <Input
+                    className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 flex-wrap gap-2.5">
+                      {field.value.map((tag: string) => (
+                        <TagCard
+                          key={tag}
+                          _id={tag}
+                          name={tag}
+                          compact
+                          remove
+                          isButton
+                          handleRemove={() => handleTagRemove(tag, field)}
+                        />
+                      ))}
+                    </div>
+                  )}
                 </div>
-               
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2.5">
-                Add up to 3 tags to describe what your question is about. You need to press Enter to add a tag.
-
+                Add up to 3 tags to describe what your question is about. You
+                need to press Enter to add a tag.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-
-
-          <div className="mt-16 flex justify-end">
-            <Button className="primary-gradient !text-light-900 w-fit">
-              Ask A Question
-
-            </Button>
-          </div>
+        <div className="mt-16 flex justify-end">
+          <Button className="primary-gradient !text-light-900 w-fit">
+            Ask A Question
+          </Button>
+        </div>
       </form>
     </Form>
   );
