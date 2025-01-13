fcaa7999d1019a00abdc62c12b6c30f1fd00d0ff
zakaria <zakariasalaheddine777@gmail.com>
2025-01-13 11:42:39 +0100

    Add GitHub Actions workflow for deployment

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -1,42 +1,44 @@
-import React from "react";
-import Link from "next/link";
 import Image from "next/image";
-import Theme from "./Theme";
-import MobileNavigation from "./MobileNavigation";
+import Link from "next/link";
+import React from "react";
+
 import { auth } from "@/auth";
+
+import MobileNavigation from "./MobileNavigation";
+import Theme from "./Theme";
 import UserAvatar from "../../UserAvatar";
 
 const Navbar = async () => {
-
   const session = await auth();
+
   return (
-    <nav className="flex-between p-6  background-light900_dark200 fixed w-full z-50 gap-5 shadow-light-300 dark:shadow-none sm:px-12  ">
-      <Link href="/" className="flex items-center gap-2">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
           width={23}
           height={23}
           alt="DevFlow Logo"
         />
-        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden ">
-          Dev <span className="text-primary-500">Flow</span>
+
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Flow</span>
         </p>
       </Link>
 
       <p>Global Search</p>
 
-      <div className="flex-between gap-5 ">
+      <div className="flex-between gap-5">
         <Theme />
 
         {session?.user?.id && (
           <UserAvatar
-          id={session?.user?.id}
-          name={session?.user?.name!}
-          imageUrl={session?.user?.image}
-
-
+            id={session.user.id}
+            name={session.user.name!}
+            imageUrl={session.user?.image!}
           />
         )}
+
         <MobileNavigation />
       </div>
     </nav>
